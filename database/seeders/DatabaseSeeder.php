a9a0e7c9c72a7e95aad989b7ff76bbe3e211e9c7
Irvin <kirvin02@gmail.com>
2024-10-18 14:43:34 -0500

    primer commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Address;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UsersSeeder::class,
+            RolesPermissionsSeeder::class,
+        ]);
+
+        \App\Models\User::factory(10)->create();
+
+        Address::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
